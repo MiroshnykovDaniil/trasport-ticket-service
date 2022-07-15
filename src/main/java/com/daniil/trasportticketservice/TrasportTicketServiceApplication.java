dc740797410af2e67944e60e5954fc04ad18f556
Daniil Miroshnykov <danielq911q@gmail.com>
2022-07-15 19:04:32 +0300

    initial

---
+++ src/main/java/com/daniil/trasportticketservice/TrasportTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.daniil.trasportticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasportTicketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrasportTicketServiceApplication.class, args);
+    }
+
+}
